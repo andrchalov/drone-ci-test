233a35c55b556d233b31d8276790267d1e0b80d0
Andrey Chalov <andrchalov@gmail.com>
2019-11-13 10:03:19 +0300

    sdf

--- auto_create_tag.sh
+++ auto_create_tag.sh
@@ -9,15 +9,15 @@ then
   sha=`cat .git/refs/heads/master`
 
   echo '{"tag":"'${current_release}'","message":"new version","object":"'${sha}'","type":"tree"}'
-  
+
   curl -v -X POST -d '{"tag":"'${current_release}'","message":"new version","object":"'${sha}'","type":"tree"}' \
     --header "Content-Type:application/json" \
-    -u andrchalov:$GITHUB_API_KEY \
+    -u ${DRONE_REPO_OWNER}:${GITHUB_API_KEY} \
     "https://api.github.com/repos/${DRONE_REPO}/git/tags"
 
   echo "https://api.github.com/repos/${DRONE_REPO}/git/tags"
 
   curl -X POST -d '{"ref":"refs/tags/'${current_release}'","sha":"'${sha}'"' \
     --header "Content-Type:application/json" \
-    -u andrchalov:$GITHUB_API_KEY "https://api.github.com/repos/${DRONE_REPO}/git/refs"
+    -u ${DRONE_REPO_OWNER}:${GITHUB_API_KEY} "https://api.github.com/repos/${DRONE_REPO}/git/refs"
 fi
