65a20d354ee5cae411e6e16f583e392aae427fb2
Andrey Chalov <andrchalov@gmail.com>
2019-11-13 19:51:53 +0300

    work

--- auto_create_tag.sh
+++ auto_create_tag.sh
@@ -8,14 +8,10 @@ if [ $tag_page_status == "404" ]
 then
   sha=`cat .git/refs/heads/master`
 
-  echo '{"tag":"'${current_release}'","message":"new version","object":"'${sha}'","type":"tree"}'
-
-  echo curl -v -X POST -d '{"tag":"'${current_release}'","message":"new version","object":"'${sha}'","type":"tree"}' \
+  curl -v -X POST -d '{"tag":"'${current_release}'","message":"new version","object":"'${sha}'","type":"tree"}' \
     --header "Content-Type:application/json" \
     -u ${DRONE_REPO_OWNER}:${GITHUB_API_KEY} \
     "https://api.github.com/repos/${DRONE_REPO}/git/tags"
-
-  echo "https://api.github.com/repos/${DRONE_REPO}/git/tags"
 
   curl -X POST -d '{"ref":"refs/tags/'${current_release}'","sha":"'${sha}'"' \
     --header "Content-Type:application/json" \
